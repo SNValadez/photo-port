df3db373ee72923563f0ee2042d9b099b26c9b39
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-03 13:33:45 -0800

    cover sources from assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
+
   return (
-    <div></div>
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
   );
 }
 
