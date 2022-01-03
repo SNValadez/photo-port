1ee0d769fc0c342dac19f2281bbbd72bb55de552
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-03 14:09:00 -0800

    map nav function

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,17 +14,23 @@ const categories = [
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ];
-  
-function Nav() {
 
+function Nav() {
+    function categorySelected() {
+        console.log("hello")
+      }
   return (
+      
     <header>
+        
   <h2>
     <a href="/">
       <span role="img" aria-label="camera"> 📸</span> Oh Snap!
     </a>
   </h2>
+  
   <nav>
+      
     <ul className="flex-row">
       <li className="mx-2">
         <a href="#about">
@@ -34,6 +40,17 @@ function Nav() {
       <li>
         <span>Contact</span>
       </li>
+
+       {categories.map((category) => (
+        <li
+          className="mx-1"
+          key={category.name}
+        >
+          <span onClick={categorySelected}>
+            {category.name}
+          </span>
+        </li>
+      ))}
     </ul>
   </nav>
 </header>
